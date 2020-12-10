dbf51aacda61e6b5e26acef5bf7dc37e9a28e1ca
Luis <skorpio19691102@freemail.hu>
2020-12-10 23:45:14 +0100

    Pont kiírás

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ let myCards = document.getElementById('container');
 let resultsArray = [];
 let counter = 0;
 let text = document.getElementById('text');
-let seconds = 00; 
-let tens = 00; 
+let seconds = 00;
+let tens = 00;
 let appendTens = document.getElementById("tens");
 let appendSeconds = document.getElementById("seconds");
-let Interval ;
+let Interval;
 let images = [
-  '1', 
-  '2', 
-  '3', 
-  '4', 
+  '1',
+  '2',
+  '3',
+  '4',
   '5'
 ];
 
@@ -19,8 +19,8 @@ let clone = images.slice(0); //array duplicate
 let cards = images.concat(clone);
 
 // Shufffel function
-function shuffle(o){
-  for(let j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i],   o[i] = o[j], o[j] = x);
+function shuffle(o) {
+  for (let j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
   return o;
 }
 shuffle(cards);
@@ -30,80 +30,81 @@ for (let i = 0; i < cards.length; i++) {
   card.dataset.item = cards[i];
   card.dataset.view = "card";
   myCards.appendChild(card);
-     
+
   card.onclick = function () {
-   
-    if (this.className != 'flipped' && this.className != 'correct'){
-        this.className = 'flipped';
-        let result = this.dataset.item;
-        resultsArray.push(result);
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);
+
+    if (this.className != 'flipped' && this.className != 'correct') {
+      this.className = 'flipped';
+      let result = this.dataset.item;
+      resultsArray.push(result);
+      clearInterval(Interval);
+      Interval = setInterval(startTimer, 10);
     }
-  
+
     if (resultsArray.length > 1) {
 
       if (resultsArray[0] === resultsArray[1]) {
         check("correct");
-        counter ++;
+        counter++;
+        text.innerHTML = "Your points: " + counter;
         win();
         resultsArray = [];
       } else {
         check("reverse");
         resultsArray = [];
       }
-      
+
     }
-    
+
   }
-   
+
 };
 
 
-let check = function(className) {
-  
-  let x = document.getElementsByClassName("flipped");
-  setTimeout(function() {
+let check = function (className) {
 
-    for(let i = (x.length - 1); i >= 0; i--) {
+  let x = document.getElementsByClassName("flipped");
+  setTimeout(function () {
+
+    for (let i = (x.length - 1); i >= 0; i--) {
       x[i].className = className;
     }
-     
-  },500);
-   
+
+  }, 500);
+
 }
 
 let win = function () {
 
-  if(counter === 5) {
+  if (counter === 5) {
     clearInterval(Interval);
-    text.innerHTML = "Your time was " + seconds + ":" + tens;
-  } 
-  
+    text.innerHTML = "Your time was: " + seconds + `:` + tens + ' Your points: ' + counter;
+
+  }
 }
-     
-function startTimer () {
-  tens++; 
-    
-  if(tens < 9){
-    appendTens.innerHTML = "0" + tens;
+
+  function startTimer() {
+    tens++;
+
+    if (tens < 9) {
+      appendTens.innerHTML = "0" + tens;
+    }
+
+    if (tens > 9) {
+      appendTens.innerHTML = tens;
+
+    }
+
+    if (tens > 99) {
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+
+    if (seconds > 9) {
+      appendSeconds.innerHTML = seconds;
+    }
+
   }
-    
-  if (tens > 9){
-    appendTens.innerHTML = tens;
-      
-  } 
-    
-  if (tens > 99) {
-    seconds++;
-    appendSeconds.innerHTML = "0" + seconds;
-    tens = 0;
-    appendTens.innerHTML = "0" + 0;
-  }
-    
-  if (seconds > 9){
-    appendSeconds.innerHTML = seconds;
-  }
-  
-}
 
